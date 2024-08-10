17d7f95fc360045115343d027bb14250689a6fa1
vviterbo <victor.viterbo@epfl.ch>
2024-08-10 12:06:09 +0200

    seems to be working for one file

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 11:48:53 by vviterbo          #+#    #+#             */
-/*   Updated: 2024/08/10 10:51:59 by vviterbo         ###   ########.fr       */
+/*   Updated: 2024/08/10 12:05:52 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_open_lines	**g_current_line = NULL;
+static t_open_lines	**g_current_lines = NULL;
 
 char				*get_next_line(int fd);
 static t_open_lines	*init_line(int fd);
@@ -23,7 +23,6 @@ char	*get_next_line(int fd)
 	char			*line;
 	char			*new_block;
 	int				n_block;
-	size_t			i;
 	t_open_lines	*current;
 
 	n_block = 1;
@@ -69,27 +68,28 @@ static t_open_lines	*init_line(int fd)
 	t_open_lines	*last;
 
 	last = NULL;
-	
-	if (g_current_line)
-		current = *g_current_line;
+	if (g_current_lines)
+	{
+		current = *g_current_lines;
+		while (current)
+		{
+			if (current->fd == fd)
+				return (current);
+			last = current;
+			current = current->next;
+		}
+	}
 	else
 	{
+		g_current_lines = malloc(sizeof(t_open_lines *));
+		if (!g_current_lines)
+			return (NULL);
 		current = malloc(sizeof(t_open_lines));
 		if (!current)
 			return (NULL);
-		g_current_line = &current;
+		*g_current_lines = current;
 		current->fd = -2;
 	}
-	while (current->fd != -2)
-	{
-		if (current->fd == fd)
-			return (current);
-		last = current;
-		current = current->next;
-	}
-	g_current_line = malloc(sizeof(t_open_lines));
-	if (!g_current_line)
-		return (NULL);
 	current->current_line = malloc(1025 * sizeof(char));
 	if (!current->current_line)
 		return (NULL);
